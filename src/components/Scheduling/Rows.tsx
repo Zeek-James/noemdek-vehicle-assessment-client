fb729fb01d5865327c34e6ec392074c30bbdc597
Isaac James <chizuruokej@yahoo.com>
2023-08-30 17:38:10 +0100

    fixs drag error

--- src/components/Scheduling/Rows.tsx
+++ src/components/Scheduling/Rows.tsx
@@ -227,7 +227,7 @@ const useScheduleRows: React.FC<ScheduleTableProps> = ({
                 >
                   <div className="flex w-full  justify-around border-b border-b-slate-300 items-center min-h-[100px]">
                     {scheduleCells}
-                    {provided.placeholder}
+                    {/* {provided.placeholder} */}
                   </div>
                 </div>
               )}
